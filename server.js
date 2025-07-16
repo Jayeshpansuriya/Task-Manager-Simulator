0b05d48ad7c3ab864656338713071191e10e9e65
Jayesh Pansuriya <pansuriyajayesh786@gmail.com>
2025-07-16 17:06:06 +0530

    "geting all the task in console termonal by hiting "http://localhost:3000/log-tasks" "

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const _dirname = path.dirname(_filename);
 
 app.use(express.static(path.join(_dirname,'public')));
 
-
 app.get('/tasks',(req,res)=>{
     const filtered = tasks
     .filter(task=>task.priority==='high' && !task.completed)
@@ -21,6 +20,37 @@ app.get('/tasks',(req,res)=>{
     }));
     res.json(filtered);
 });
+
+function logTasksWithDelay(task,callback){
+    tasks.forEach((task,index)=>{
+        setTimeout(()=>{
+
+            console.log(`task: ${task.title}-[${task.priority}]`);
+            if(index=== tasks.length-1){
+                callback();
+            }
+
+        },index*1000);
+    });
+}
+
+app.get("/log-tasks",async(req,res)=>{
+    const filtered=tasks.filter(task=>task.priority==='high' && !task.completed)
+    .map(task=>({
+        ...task,
+        title:task.title.toUpperCase(),
+    }));
+    logTasksWithDelay(filtered,()=>{
+        console.log("✅ Logging complete!")
+    });
+    res.send("⏳ Logging started on server console… Check terminal.")
+})
+
+
+
+
+
+
 
 app.listen(PORT,()=>{
     console.log(`🚀 Server running at http://localhost:${PORT}`)
